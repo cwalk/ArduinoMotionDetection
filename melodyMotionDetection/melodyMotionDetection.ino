9a22b29dbdbcd3b8ca7653b33ade700c6ad79b02
Clayton Walker <claytonwalkerucf@knights.ucf.edu>
2015-12-30 18:19:03 -0500

    Will play melody and light up LED when PIR sensor detects motion

---
+++ melodyMotionDetection/melodyMotionDetection.ino
@@ -0,0 +1,54 @@
+/*
+ * Melody played when motion detected
+ * Clayton Walker
+ */
+#include "pitches.h"
+
+int ledPin = 13;
+int pirPin = 8;
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  //setup pin modes
+  pinMode(ledPin, OUTPUT);
+  pinMode(pirPin, INPUT);
+
+  delay(3000);
+}
+
+void loop() {
+  int val = digitalRead(pirPin);
+  
+  if(val == HIGH){
+    digitalWrite(ledPin, HIGH);
+
+    // iterate over the notes of the melody:
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+  
+      // to calculate the note duration, take one second
+      // divided by the note type.
+      //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+      int noteDuration = 1000 / noteDurations[thisNote];
+      tone(8, melody[thisNote], noteDuration);
+  
+      // to distinguish the notes, set a minimum time between them.
+      // the note's duration + 30% seems to work well:
+      int pauseBetweenNotes = noteDuration * 1.30;
+      delay(pauseBetweenNotes);
+      // stop the tone playing:
+      noTone(8);
+    }
+  }
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+}
